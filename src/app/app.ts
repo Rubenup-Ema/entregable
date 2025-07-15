31d87c8d9fecbc019d587e46595c4e97739e2f0e
Ruben Dobobuto <rdobobuto@emaresa.cl>
2025-07-15 19:02:51 -0400

    Conponentes nuevos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./navbar/navbar";
+import { Toolbar } from "./toolbar/toolbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Toolbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
